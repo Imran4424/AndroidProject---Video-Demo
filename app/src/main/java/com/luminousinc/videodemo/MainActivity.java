a0acdf949d4cfe6626dc42cd4a579d8bdbe41941
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-29 21:07:41 +0600

    full project work

---
+++ app/src/main/java/com/luminousinc/videodemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.luminousinc.videodemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    VideoView mainVideo;
+    MediaController mainMediaController;
+    View decorView;
+    int uiOptions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        mainVideo = (VideoView) findViewById(R.id.videoView);
+        mainVideo.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.epictrailer);
+
+        mainMediaController = new MediaController(this);
+        mainMediaController.setAnchorView(mainVideo);
+        mainVideo.setMediaController(mainMediaController);
+
+        mainVideo.start();
+    }
+}
