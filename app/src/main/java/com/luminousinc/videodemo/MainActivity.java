1a79ab4c71cc565e9e1cbde8cbdcec8224262052
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-29 21:26:58 +0600

    implement hide system UI

--- app/src/main/java/com/luminousinc/videodemo/MainActivity.java
+++ app/src/main/java/com/luminousinc/videodemo/MainActivity.java
@@ -10,18 +10,12 @@ import android.widget.VideoView;
 public class MainActivity extends AppCompatActivity {
     VideoView mainVideo;
     MediaController mainMediaController;
-    View decorView;
-    int uiOptions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        decorView = getWindow().getDecorView();
-        // Hide the status bar.
-        uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
 
         mainVideo = (VideoView) findViewById(R.id.videoView);
         mainVideo.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.epictrailer);
@@ -36,10 +30,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-
-        decorView = getWindow().getDecorView();
-        // Hide the status bar.
-        uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
+        hideSystemUI();
     }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    
+
 }
