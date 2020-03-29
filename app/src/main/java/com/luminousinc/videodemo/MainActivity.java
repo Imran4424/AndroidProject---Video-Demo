570933e6728929534fbc6026ce4a9f984e6d50f6
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-29 21:28:35 +0600

    implement hide system UI

--- app/src/main/java/com/luminousinc/videodemo/MainActivity.java
+++ app/src/main/java/com/luminousinc/videodemo/MainActivity.java
@@ -50,13 +50,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // Shows the system bars by removing all the flags
-    // except for the ones that make the content appear under the system bars.
-    private void showSystemUI() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-    }
+    
 }
